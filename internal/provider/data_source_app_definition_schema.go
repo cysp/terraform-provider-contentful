471cca278933a5a095bf53abe4c278045706145f
Scott Talbot <s@chikachow.org>
2024-02-29 17:22:46 +1100

    feat(provider): introduce app definition datasource

--- internal/provider/data_source_app_definition_schema.go
+++ internal/provider/data_source_app_definition_schema.go
@@ -4,155 +4,138 @@ import (
 	"context"
 
 	"github.com/hashicorp/terraform-plugin-framework-jsontypes/jsontypes"
-	"github.com/hashicorp/terraform-plugin-framework/resource/schema"
-	"github.com/hashicorp/terraform-plugin-framework/resource/schema/planmodifier"
-	"github.com/hashicorp/terraform-plugin-framework/resource/schema/stringplanmodifier"
+	"github.com/hashicorp/terraform-plugin-framework/datasource/schema"
 )
 
-func AppDefinitionResourceSchema(ctx context.Context) schema.Schema {
+func AppDefinitionDataSourceSchema(ctx context.Context) schema.Schema {
 	return schema.Schema{
 		Attributes: map[string]schema.Attribute{
 			"id": schema.StringAttribute{
 				Computed: true,
-				PlanModifiers: []planmodifier.String{
-					UseStateForUnknown(),
-				},
 			},
 			"organization_id": schema.StringAttribute{
 				Required: true,
-				PlanModifiers: []planmodifier.String{
-					stringplanmodifier.RequiresReplace(),
-				},
 			},
 			"app_definition_id": schema.StringAttribute{
-				Computed: true,
-				PlanModifiers: []planmodifier.String{
-					UseStateForUnknown(),
-				},
-			},
-			"name": schema.StringAttribute{
 				Required: true,
 			},
-			"src": schema.StringAttribute{
-				Optional: true,
+			"name": schema.StringAttribute{
 				Computed: true,
-				PlanModifiers: []planmodifier.String{
-					UseStateForUnknown(),
-				},
+			},
+			"src": schema.StringAttribute{
+				Computed: true,
 			},
 			"bundle_id": schema.StringAttribute{
-				Optional: true,
 				Computed: true,
-				PlanModifiers: []planmodifier.String{
-					UseStateForUnknown(),
-				},
 			},
 			"locations": schema.ListNestedAttribute{
 				NestedObject: schema.NestedAttributeObject{
 					Attributes: map[string]schema.Attribute{
 						"location": schema.StringAttribute{
-							Required: true,
+							Computed: true,
 						},
 						"field_types": schema.ListNestedAttribute{
 							NestedObject: schema.NestedAttributeObject{
 								Attributes: map[string]schema.Attribute{
 									"type": schema.StringAttribute{
-										Required: true,
+										Computed: true,
 									},
 									"link_type": schema.StringAttribute{
-										Optional: true,
+										Computed: true,
 									},
-									"items": schema.SingleNestedAttribute{
-										Attributes: map[string]schema.Attribute{
-											"type": schema.StringAttribute{
-												Required: true,
-											},
-											"link_type": schema.StringAttribute{
-												Optional: true,
+									"items": schema.ListNestedAttribute{
+										NestedObject: schema.NestedAttributeObject{
+											Attributes: map[string]schema.Attribute{
+												"type": schema.StringAttribute{
+													Computed: true,
+												},
+												"link_type": schema.StringAttribute{
+													Computed: true,
+												},
 											},
 										},
-										Optional: true,
+										Computed: true,
 									},
 								},
 							},
-							Optional: true,
+							Computed: true,
 						},
 						"navigation_item": schema.SingleNestedAttribute{
 							Attributes: map[string]schema.Attribute{
 								"name": schema.StringAttribute{
-									Required: true,
+									Computed: true,
 								},
 								"path": schema.StringAttribute{
-									Required: true,
+									Computed: true,
 								},
 							},
-							Optional: true,
+							Computed: true,
 						},
 					},
 				},
-				Required: true,
+				Computed: true,
 			},
 			"parameters": schema.SingleNestedAttribute{
 				Attributes: map[string]schema.Attribute{
 					"installation": schema.ListNestedAttribute{
 						NestedObject: schema.NestedAttributeObject{
-							Attributes: AppDefinitionParameterSchemaAttributes(ctx),
+							Attributes: AppDefinitionParameterDataSourceSchemaAttributes(ctx),
 						},
-						Optional: true,
+						Computed: true,
 					},
 					"instance": schema.ListNestedAttribute{
 						NestedObject: schema.NestedAttributeObject{
-							Attributes: AppDefinitionParameterSchemaAttributes(ctx),
+							Attributes: AppDefinitionParameterDataSourceSchemaAttributes(ctx),
 						},
-						Optional: true,
+						Computed: true,
 					},
 				},
-				Optional: true,
+				Computed: true,
 			},
 		},
 	}
 }
 
 //nolint:dupl
-func AppDefinitionParameterSchemaAttributes(ctx context.Context) map[string]schema.Attribute {
+func AppDefinitionParameterDataSourceSchemaAttributes(ctx context.Context) map[string]schema.Attribute {
 	return map[string]schema.Attribute{
 		"id": schema.StringAttribute{
-			Required: true,
+			Computed: true,
 		},
 		"type": schema.StringAttribute{
-			Required: true,
+			Computed: true,
 		},
 		"name": schema.StringAttribute{
-			Required: true,
+			Computed: true,
 		},
 		"description": schema.StringAttribute{
-			Optional: true,
+			Computed: true,
 		},
 		"required": schema.BoolAttribute{
-			Optional: true,
+			Computed: true,
 		},
 		"default": schema.StringAttribute{
 			CustomType: jsontypes.NormalizedType{},
-			Optional:   true,
+			Computed:   true,
 		},
 		"options": schema.ListAttribute{
 			ElementType: jsontypes.NormalizedType{},
 			CustomType:  NewTypedListNull[jsontypes.Normalized](ctx).CustomType(ctx),
-			Optional:    true,
+			Computed:    true,
 		},
 		"labels": schema.SingleNestedAttribute{
 			Attributes: map[string]schema.Attribute{
 				"empty": schema.StringAttribute{
-					Optional: true,
+					Computed: true,
 				},
 				"true": schema.StringAttribute{
-					Optional: true,
+					Computed: true,
 				},
 				"false": schema.StringAttribute{
-					Optional: true,
+					Computed: true,
 				},
 			},
-			Optional: true,
+			Computed: true,
 		},
 	}
 }
---
+++ internal/provider/testdata/TestAccAppDefinitionDataSource/main.tf
@@ -0,0 +1,4 @@
+data "contentful_app_definition" "test" {
+  organization_id   = var.organization_id
+  app_definition_id = var.app_definition_id
+}
