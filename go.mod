cysp/terraform-provider-typesense

chore: go mod init

go.mod
@@ -1,0 +1,3 @@
+module github.com/cysp/terraform-provider-typesense
+
+go 1.22.1

cysp/terraform-provider-contentful

go.mod
@@ -1,0 +1,3 @@
+module github.com/cysp/terraform-provider-contentful
+
+go 1.22.1