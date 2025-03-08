ae2ede1eaa82776e2c0399200f303ff56e88e110
Scott Talbot <s@chikachow.org>
2025-03-08 17:53:03 +1100

    ref(provider/preview_api_key): use hand-rolled model type

---
+++ internal/provider/preview_api_key_model_response.go
@@ -0,0 +1,29 @@
+package provider
+
+import (
+	"context"
+
+	cm "github.com/cysp/terraform-provider-contentful/internal/contentful-management-go"
+	"github.com/cysp/terraform-provider-contentful/internal/provider/util"
+	"github.com/hashicorp/terraform-plugin-framework/diag"
+	"github.com/hashicorp/terraform-plugin-framework/path"
+	"github.com/hashicorp/terraform-plugin-framework/types"
+)
+
+func (model *PreviewAPIKeyModel) ReadFromResponse(ctx context.Context, previewAPIKey *cm.PreviewApiKey) diag.Diagnostics {
+	diags := diag.Diagnostics{}
+
+	model.PreviewAPIKeyID = types.StringValue(previewAPIKey.Sys.ID)
+
+	model.Name = types.StringValue(previewAPIKey.Name)
+	model.Description = util.OptNilStringToStringValue(previewAPIKey.Description)
+
+	model.AccessToken = types.StringValue(previewAPIKey.AccessToken)
+
+	environmentsList, environmentsListDiags := NewEnvironmentIDsListValueFromEnvironmentLinks(ctx, path.Root("environments"), previewAPIKey.Environments)
+	diags.Append(environmentsListDiags...)
+
+	model.Environments = environmentsList
+
+	return diags
+}
