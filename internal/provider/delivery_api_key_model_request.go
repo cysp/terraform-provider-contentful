cf2f5e5cede55b9fbf299c66054b682f323cdc8c
Scott Talbot <s@chikachow.org>
2025-03-08 17:04:54 +1100

    ref(provider/delivery_api_key): use hand-rolled model type

---
+++ internal/provider/delivery_api_key_model_request.go
@@ -0,0 +1,26 @@
+package provider
+
+import (
+	"context"
+
+	cm "github.com/cysp/terraform-provider-contentful/internal/contentful-management-go"
+	"github.com/cysp/terraform-provider-contentful/internal/provider/util"
+	"github.com/hashicorp/terraform-plugin-framework/diag"
+	"github.com/hashicorp/terraform-plugin-framework/path"
+)
+
+func (model *DeliveryAPIKeyModel) ToAPIKeyRequestFields(ctx context.Context) (cm.ApiKeyRequestFields, diag.Diagnostics) {
+	diags := diag.Diagnostics{}
+
+	req := cm.ApiKeyRequestFields{
+		Name:        model.Name.ValueString(),
+		Description: util.StringValueToOptNilString(model.Description),
+	}
+
+	environments, environmentsDiags := ToEnvironmentLinks(ctx, path.Root("environments"), model.Environments)
+	diags.Append(environmentsDiags...)
+
+	req.Environments = environments
+
+	return req, diags
+}
