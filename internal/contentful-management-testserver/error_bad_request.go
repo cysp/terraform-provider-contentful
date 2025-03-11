538098b333acb988422f56f3dcbef9fa66ca01e5
Scott Talbot <s@chikachow.org>
2025-03-12 00:43:38 +1100

    chore(contentful-management-testserver): remove unused error response code

--- internal/contentful-management-testserver/error_bad_request.go
+++ internal/contentful-management-testserver/error_bad_request.go
@@ -4,18 +4,14 @@ import (
 	"net/http"
 )
 
-func NewContentfulManagementErrorBadRequestHandler() *ContentfulManagementErrorHandler {
-	return NewContentfulManagementErrorHandler(http.StatusBadRequest, "BadRequest", pointerTo("The request was malformed or contained invalid parameters."), nil)
-}
-
 func WriteContentfulManagementErrorBadRequestResponse(w http.ResponseWriter) error {
 	return WriteContentfulManagementErrorResponse(w, http.StatusBadRequest, "BadRequest", pointerTo("The request was malformed or contained invalid parameters."), nil)
 }
 
-func WriteContentfulManagementErrorBadRequestResponseWithError(w http.ResponseWriter, err error) error {
-	return WriteContentfulManagementErrorBadRequestResponseWithDetails(w, err.Error())
-}
-
 func WriteContentfulManagementErrorBadRequestResponseWithDetails(w http.ResponseWriter, details string) error {
 	return WriteContentfulManagementErrorResponse(w, http.StatusBadRequest, "BadRequest", pointerTo("The request was malformed or contained invalid parameters."), []byte(details))
+}
+
+func WriteContentfulManagementErrorBadRequestResponseWithError(w http.ResponseWriter, err error) error {
+	return WriteContentfulManagementErrorBadRequestResponseWithDetails(w, err.Error())
 }
