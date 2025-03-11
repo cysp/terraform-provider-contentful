7d266c562a49c8d1f62145c474f761ac802ad3b6
Scott Talbot <s@chikachow.org>
2025-03-12 00:43:38 +1100

    chore(contentful-management-testserver): remove unused error response code

--- internal/contentful-management-testserver/error_not_found.go
+++ internal/contentful-management-testserver/error_not_found.go
@@ -4,10 +4,6 @@ import (
 	"net/http"
 )
 
-func NewContentfulManagementErrorNotFoundHandler() *ContentfulManagementErrorHandler {
-	return NewContentfulManagementErrorHandler(http.StatusNotFound, "NotFound", pointerTo("The resource could not be found."), nil)
-}
-
 func WriteContentfulManagementErrorNotFoundResponse(w http.ResponseWriter) error {
 	return WriteContentfulManagementErrorResponse(w, http.StatusNotFound, "NotFound", pointerTo("The resource could not be found."), nil)
 }
