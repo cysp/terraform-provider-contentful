cea71a913acc62fb2574d6dc52db4a09e319fc9f
Scott Talbot <s@chikachow.org>
2025-03-13 22:21:21 +1100

    ref(provider): derive object attr types from schema attributes

--- internal/provider/content_type_field_items_value.go
+++ internal/provider/content_type_field_items_value.go
@@ -75,16 +75,12 @@ func (v ContentTypeFieldItemsValue) SchemaAttributes(_ context.Context) map[stri
 
 //nolint:ireturn
 func (v ContentTypeFieldItemsValue) CustomType(ctx context.Context) basetypes.ObjectTypable {
-	return ContentTypeFieldItemsType{
-		v.ObjectType(ctx),
-	}
+	return ContentTypeFieldItemsType{ObjectType: v.ObjectType(ctx)}
 }
 
 //nolint:ireturn
 func (v ContentTypeFieldItemsValue) Type(ctx context.Context) attr.Type {
-	return ContentTypeFieldItemsType{
-		ObjectType: v.ObjectType(ctx),
-	}
+	return v.CustomType(ctx)
 }
 
 func (v ContentTypeFieldItemsValue) ObjectType(ctx context.Context) basetypes.ObjectType {
@@ -93,12 +89,8 @@ func (v ContentTypeFieldItemsValue) ObjectType(ctx context.Context) basetypes.Ob
 	}
 }
 
-func (v ContentTypeFieldItemsValue) ObjectAttrTypes(_ context.Context) map[string]attr.Type {
-	return map[string]attr.Type{
-		"type":        basetypes.StringType{},
-		"link_type":   basetypes.StringType{},
-		"validations": basetypes.ListType{ElemType: jsontypes.NormalizedType{}},
-	}
+func (v ContentTypeFieldItemsValue) ObjectAttrTypes(ctx context.Context) map[string]attr.Type {
+	return ObjectAttrTypesFromSchemaAttributes(ctx, v.SchemaAttributes(ctx))
 }
 
 func (v ContentTypeFieldItemsValue) Equal(o attr.Value) bool {
