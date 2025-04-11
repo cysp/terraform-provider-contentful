d810b97f3fbd160b89ab84e70c2d101c10f3bc82
Scott Talbot <s@chikachow.org>
2025-04-11 22:54:52 +1000

    feat(provider): implement generic typed list value type

---
+++ internal/provider/typed_list_type.go
@@ -0,0 +1,148 @@
+package provider
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/hashicorp/terraform-plugin-framework/attr"
+	"github.com/hashicorp/terraform-plugin-framework/diag"
+	"github.com/hashicorp/terraform-plugin-framework/types/basetypes"
+	"github.com/hashicorp/terraform-plugin-go/tftypes"
+)
+
+type TypedListType[T attr.Value] struct {
+	elementType attr.Type
+}
+
+var (
+	_ attr.Type                = (*TypedListType[attr.Value])(nil)
+	_ attr.TypeWithElementType = (*TypedListType[attr.Value])(nil)
+	_ basetypes.ListTypable    = (*TypedListType[attr.Value])(nil)
+)
+
+//nolint:ireturn
+func (t TypedListType[T]) TerraformType(ctx context.Context) tftypes.Type {
+	var v T
+
+	return tftypes.List{ElementType: v.Type(ctx).TerraformType(ctx)}
+}
+
+//nolint:ireturn
+func (t TypedListType[T]) ValueFromTerraform(ctx context.Context, tfval tftypes.Value) (attr.Value, error) {
+	if tfval.Type() == nil {
+		return NewTypedListNull[T](ctx), nil
+	}
+
+	elementType := t.ElementTypeWithContext(ctx)
+
+	if !tfval.Type().Equal(t.TerraformType(ctx)) {
+		//nolint:err113
+		return nil, fmt.Errorf("can't use %s as value of TypedList[%T], can only use %s values", tfval.String(), elementType, elementType.TerraformType(ctx).String())
+	}
+
+	if !tfval.IsKnown() {
+		return NewTypedListUnknown[T](ctx), nil
+	}
+
+	if tfval.IsNull() {
+		return NewTypedListNull[T](ctx), nil
+	}
+
+	tfelems := []tftypes.Value{}
+
+	tfelemsErr := tfval.As(&tfelems)
+	if tfelemsErr != nil {
+		return nil, fmt.Errorf("error extracting elements from terraform value: %w", tfelemsErr)
+	}
+
+	elements := make([]T, len(tfelems))
+
+	for idx, elem := range tfelems {
+		attrval, attrvalErr := elementType.ValueFromTerraform(ctx, elem)
+		if attrvalErr != nil {
+			return nil, fmt.Errorf("error converting element from terraform value: %w", attrvalErr)
+		}
+
+		element, elementOk := attrval.(T)
+		if !elementOk {
+			//nolint:err113
+			return nil, fmt.Errorf("can't use %s as value of TypedList[%T], can only use %s values", element.String(), elementType, elementType.TerraformType(ctx).String())
+		}
+
+		elements[idx] = element
+	}
+
+	list, listDiags := NewTypedList(ctx, elements)
+
+	return list, ErrorFromDiags(listDiags)
+}
+
+//nolint:ireturn
+func (t TypedListType[T]) ValueType(context.Context) attr.Value {
+	return TypedList[T]{}
+}
+
+func (t TypedListType[T]) Equal(o attr.Type) bool {
+	other, ok := o.(TypedListType[T])
+	if !ok {
+		return false
+	}
+
+	elementType := t.ElementType()
+	otherElementType := other.ElementType()
+
+	if elementType == nil && otherElementType == nil {
+		return true
+	}
+
+	return elementType.Equal(otherElementType)
+}
+
+func (t TypedListType[T]) String() string {
+	var v T
+
+	return fmt.Sprintf("TypedList[%T]", v)
+}
+
+func (t TypedListType[T]) ApplyTerraform5AttributePathStep(step tftypes.AttributePathStep) (any, error) {
+	var v T
+
+	if _, ok := step.(tftypes.ElementKeyInt); !ok {
+		//nolint:err113
+		return nil, fmt.Errorf("cannot apply step %T to TypedList[%T]", step, v)
+	}
+
+	return t.ElementType(), nil
+}
+
+//nolint:ireturn
+func (t TypedListType[T]) WithElementType(typ attr.Type) attr.TypeWithElementType {
+	return TypedListType[attr.Value]{elementType: typ}
+}
+
+//nolint:ireturn
+func (t TypedListType[T]) ElementType() attr.Type {
+	return t.ElementTypeWithContext(context.Background())
+}
+
+//nolint:ireturn
+func (t TypedListType[T]) ElementTypeWithContext(ctx context.Context) attr.Type {
+	if t.elementType != nil {
+		return t.elementType
+	}
+
+	var v T
+
+	return v.Type(ctx)
+}
+
+//nolint:ireturn
+func (t TypedListType[T]) ValueFromList(ctx context.Context, value basetypes.ListValue) (basetypes.ListValuable, diag.Diagnostics) {
+	var diags diag.Diagnostics
+
+	var elements []T
+
+	diags.Append(value.ElementsAs(ctx, &elements, false)...)
+
+	return TypedList[T]{elements: elements, state: attr.ValueStateKnown}, diags
+}
---
+++ internal/provider/typed_list_type_test.go
@@ -0,0 +1,107 @@
+package provider_test
+
+import (
+	"testing"
+
+	"github.com/cysp/terraform-provider-contentful/internal/provider"
+	"github.com/hashicorp/terraform-plugin-framework/types"
+	"github.com/hashicorp/terraform-plugin-go/tftypes"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestTypedListTypeValueFromTerraform(t *testing.T) {
+	t.Parallel()
+
+	ctx := t.Context()
+
+	listType := provider.TypedList[types.String]{}.Type(ctx)
+
+	testcases := map[string]struct {
+		tfval       tftypes.Value
+		expectError bool
+		expected    provider.TypedList[types.String]
+	}{
+		"null type": {
+			tfval:    tftypes.NewValue(nil, nil),
+			expected: provider.NewTypedListNull[types.String](ctx),
+		},
+		"unknown": {
+			tfval:    tftypes.NewValue(tftypes.List{ElementType: tftypes.String}, tftypes.UnknownValue),
+			expected: provider.NewTypedListUnknown[types.String](ctx),
+		},
+		"null": {
+			tfval:    tftypes.NewValue(tftypes.List{ElementType: tftypes.String}, nil),
+			expected: provider.NewTypedListNull[types.String](ctx),
+		},
+		"incorrect type": {
+			tfval:       tftypes.NewValue(tftypes.String, "string"),
+			expectError: true,
+		},
+		"incorrect element type": {
+			tfval:       tftypes.NewValue(tftypes.List{ElementType: tftypes.Number}, []tftypes.Value{}),
+			expectError: true,
+		},
+		"empty": {
+			tfval:    tftypes.NewValue(tftypes.List{ElementType: tftypes.String}, []tftypes.Value{}),
+			expected: DiagsNoErrorsMust(provider.NewTypedList(ctx, []types.String{})),
+		},
+		"with elements": {
+			tfval: tftypes.NewValue(tftypes.List{ElementType: tftypes.String}, []tftypes.Value{
+				tftypes.NewValue(tftypes.String, "value1"),
+				tftypes.NewValue(tftypes.String, "value2"),
+			}),
+			expected: DiagsNoErrorsMust(provider.NewTypedList(ctx, []types.String{
+				types.StringValue("value1"),
+				types.StringValue("value2"),
+			})),
+		},
+		"with interior unknown element": {
+			tfval: tftypes.NewValue(tftypes.List{ElementType: tftypes.String}, []tftypes.Value{
+				tftypes.NewValue(tftypes.String, "value1"),
+				tftypes.NewValue(tftypes.String, tftypes.UnknownValue),
+				tftypes.NewValue(tftypes.String, "value2"),
+			}),
+			expected: DiagsNoErrorsMust(provider.NewTypedList(ctx, []types.String{
+				types.StringValue("value1"),
+				types.StringUnknown(),
+				types.StringValue("value2"),
+			})),
+		},
+		"with interior null element": {
+			tfval: tftypes.NewValue(tftypes.List{ElementType: tftypes.String}, []tftypes.Value{
+				tftypes.NewValue(tftypes.String, "value1"),
+				tftypes.NewValue(tftypes.String, nil),
+				tftypes.NewValue(tftypes.String, "value2"),
+			}),
+			expected: DiagsNoErrorsMust(provider.NewTypedList(ctx, []types.String{
+				types.StringValue("value1"),
+				types.StringNull(),
+				types.StringValue("value2"),
+			})),
+		},
+	}
+
+	for name, testcase := range testcases {
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+
+			ctx := t.Context()
+
+			actual, err := listType.ValueFromTerraform(ctx, testcase.tfval)
+
+			if testcase.expectError {
+				assert.Error(t, err)
+
+				return
+			}
+
+			require.NoError(t, err)
+
+			actualTypedList, ok := actual.(provider.TypedList[types.String])
+			require.True(t, ok, "Expected TypedList[types.String] but got %T", actual)
+
+			assert.Equal(t, testcase.expected, actualTypedList)
+		})
+	}
+}
