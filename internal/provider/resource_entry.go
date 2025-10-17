f07d42125a8291b378c981168ae86c0a003ec6c0
Scott Talbot <s@chikachow.org>
2025-10-17 22:35:57 +1100

    res

--- internal/provider/resource_entry.go
+++ internal/provider/resource_entry.go
@@ -2,16 +2,21 @@ package provider
 
 import (
 	"context"
-	"fmt"
+	"net/http"
 
-	contentfulmanagement "github.com/cysp/terraform-provider-contentful/internal/contentful-management-go"
+	cm "github.com/cysp/terraform-provider-contentful/internal/contentful-management-go"
+	"github.com/cysp/terraform-provider-contentful/internal/provider/util"
+	"github.com/hashicorp/terraform-plugin-framework/path"
 	"github.com/hashicorp/terraform-plugin-framework/resource"
-	"github.com/hashicorp/terraform-plugin-framework/resource/schema"
+	"github.com/hashicorp/terraform-plugin-framework/resource/identityschema"
+	"github.com/hashicorp/terraform-plugin-log/tflog"
 )
 
 var (
-	_ resource.Resource              = &EntryResource{}
-	_ resource.ResourceWithConfigure = &EntryResource{}
+	_ resource.Resource                = &EntryResource{}
+	_ resource.ResourceWithConfigure   = &EntryResource{}
+	_ resource.ResourceWithIdentity    = &EntryResource{}
+	_ resource.ResourceWithImportState = &EntryResource{}
 )
 
 func NewEntryResource() resource.Resource {
@@ -19,7 +24,7 @@ func NewEntryResource() resource.Resource {
 }
 
 type EntryResource struct {
-	client *contentfulmanagement.Client
+	providerData ContentfulProviderData
 }
 
 func (r *EntryResource) Metadata(_ context.Context, req resource.MetadataRequest, resp *resource.MetadataResponse) {
@@ -31,34 +36,212 @@ func (r *EntryResource) Schema(ctx context.Context, _ resource.SchemaRequest, re
 }
 
 func (r *EntryResource) Configure(_ context.Context, req resource.ConfigureRequest, resp *resource.ConfigureResponse) {
-	if req.ProviderData == nil {
-		return
-	}
+	resp.Diagnostics.Append(SetProviderDataFromResourceConfigureRequest(req, &r.providerData)...)
+}
 
-	client, ok := req.ProviderData.(*contentfulmanagement.Client)
-	if !ok {
-		resp.Diagnostics.AddError(
-			"Unexpected Resource Configure Type",
-			fmt.Sprintf("Expected *contentfulmanagement.Client, got: %T. Please report this issue to the provider developers.", req.ProviderData),
-		)
-		return
+func (r *EntryResource) IdentitySchema(_ context.Context, _ resource.IdentitySchemaRequest, resp *resource.IdentitySchemaResponse) {
+	resp.IdentitySchema = identityschema.Schema{
+		Attributes: map[string]identityschema.Attribute{
+			"space_id":       identityschema.StringAttribute{RequiredForImport: true},
+			"environment_id": identityschema.StringAttribute{RequiredForImport: true},
+			"entry_id":       identityschema.StringAttribute{RequiredForImport: true},
+		},
 	}
+}
 
-	r.client = client
+func (r *EntryResource) ImportState(ctx context.Context, req resource.ImportStateRequest, resp *resource.ImportStateResponse) {
+	ImportStatePassthroughMultipartID(ctx, []path.Path{
+		path.Root("space_id"),
+		path.Root("environment_id"),
+		path.Root("entry_id"),
+	}, req, resp)
 }
 
 func (r *EntryResource) Create(ctx context.Context, req resource.CreateRequest, resp *resource.CreateResponse) {
-	// TODO: Implement
+	var data EntryModel
+	resp.Diagnostics.Append(req.Plan.Get(ctx, &data)...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	request, requestDiags := data.ToEntryRequestFields(ctx)
+	resp.Diagnostics.Append(requestDiags...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	params := cm.PutEntryParams{
+		SpaceID:       data.SpaceID.ValueString(),
+		EnvironmentID: data.EnvironmentID.ValueString(),
+		EntryID:       data.EntryID.ValueString(),
+		ContentTypeID: data.ContentTypeID.ValueString(),
+	}
+
+	response, err := r.providerData.client.PutEntry(ctx, request, params)
+
+	tflog.Info(ctx, "entry.create", map[string]interface{}{
+		"params":   params,
+		"request":  request,
+		"response": response,
+		"err":      err,
+	})
+
+	switch response := response.(type) {
+	case *cm.EntryStatusCode:
+		responseModel, responseModelDiags := NewEntryResourceModelFromResponse(ctx, *response)
+		resp.Diagnostics.Append(responseModelDiags...)
+		data = responseModel
+
+	default:
+		resp.Diagnostics.AddError("Failed to create entry", util.ErrorDetailFromContentfulManagementResponse(response, err))
+	}
+
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	var identityModel EntryIdentityModel
+	resp.Diagnostics.Append(CopyAttributeValues(ctx, &identityModel, &data)...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	resp.Diagnostics.Append(resp.Identity.Set(ctx, &identityModel)...)
+	resp.Diagnostics.Append(resp.State.Set(ctx, &data)...)
 }
 
 func (r *EntryResource) Read(ctx context.Context, req resource.ReadRequest, resp *resource.ReadResponse) {
-	// TODO: Implement
+	var data EntryModel
+	resp.Diagnostics.Append(req.State.Get(ctx, &data)...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	params := cm.GetEntryParams{
+		SpaceID:       data.SpaceID.ValueString(),
+		EnvironmentID: data.EnvironmentID.ValueString(),
+		EntryID:       data.EntryID.ValueString(),
+	}
+
+	response, err := r.providerData.client.GetEntry(ctx, params)
+
+	tflog.Info(ctx, "entry.read", map[string]interface{}{
+		"params":   params,
+		"response": response,
+		"err":      err,
+	})
+
+	switch response := response.(type) {
+	case *cm.Entry:
+		responseModel, responseModelDiags := NewEntryResourceModelFromResponse(ctx, *response)
+		resp.Diagnostics.Append(responseModelDiags...)
+		data = responseModel
+
+	default:
+		if res, ok := response.(cm.StatusCodeResponse); ok && res.GetStatusCode() == http.StatusNotFound {
+			resp.Diagnostics.AddWarning("Failed to read entry", util.ErrorDetailFromContentfulManagementResponse(response, err))
+			resp.State.RemoveResource(ctx)
+			return
+		}
+		resp.Diagnostics.AddError("Failed to read entry", util.ErrorDetailFromContentfulManagementResponse(response, err))
+	}
+
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	var identityModel EntryIdentityModel
+	resp.Diagnostics.Append(CopyAttributeValues(ctx, &identityModel, &data)...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	resp.Diagnostics.Append(resp.Identity.Set(ctx, &identityModel)...)
+	resp.Diagnostics.Append(resp.State.Set(ctx, &data)...)
 }
 
 func (r *EntryResource) Update(ctx context.Context, req resource.UpdateRequest, resp *resource.UpdateResponse) {
-	// TODO: Implement
+	var data EntryModel
+	resp.Diagnostics.Append(req.Plan.Get(ctx, &data)...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	request, requestDiags := data.ToEntryRequestFields(ctx)
+	resp.Diagnostics.Append(requestDiags...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	params := cm.PutEntryParams{
+		SpaceID:       data.SpaceID.ValueString(),
+		EnvironmentID: data.EnvironmentID.ValueString(),
+		EntryID:       data.EntryID.ValueString(),
+		ContentTypeID: data.ContentTypeID.ValueString(),
+	}
+
+	response, err := r.providerData.client.PutEntry(ctx, request, params)
+
+	tflog.Info(ctx, "entry.update", map[string]interface{}{
+		"params":   params,
+		"request":  request,
+		"response": response,
+		"err":      err,
+	})
+
+	switch response := response.(type) {
+	case *cm.EntryStatusCode:
+		responseModel, responseModelDiags := NewEntryResourceModelFromResponse(ctx, *response)
+		resp.Diagnostics.Append(responseModelDiags...)
+		data = responseModel
+
+	default:
+		resp.Diagnostics.AddError("Failed to update entry", util.ErrorDetailFromContentfulManagementResponse(response, err))
+	}
+
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	var identityModel EntryIdentityModel
+	resp.Diagnostics.Append(CopyAttributeValues(ctx, &identityModel, &data)...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	resp.Diagnostics.Append(resp.Identity.Set(ctx, &identityModel)...)
+	resp.Diagnostics.Append(resp.State.Set(ctx, &data)...)
 }
 
 func (r *EntryResource) Delete(ctx context.Context, req resource.DeleteRequest, resp *resource.DeleteResponse) {
-	// TODO: Implement
+	var data EntryModel
+	resp.Diagnostics.Append(req.State.Get(ctx, &data)...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	params := cm.DeleteEntryParams{
+		SpaceID:       data.SpaceID.ValueString(),
+		EnvironmentID: data.EnvironmentID.ValueString(),
+		EntryID:       data.EntryID.ValueString(),
+	}
+
+	response, err := r.providerData.client.DeleteEntry(ctx, params)
+
+	tflog.Info(ctx, "entry.delete", map[string]interface{}{
+		"params":   params,
+		"response": response,
+		"err":      err,
+	})
+
+	switch response := response.(type) {
+	case *cm.NoContent:
+		// Success
+	default:
+		if res, ok := response.(cm.StatusCodeResponse); ok && res.GetStatusCode() == http.StatusNotFound {
+			resp.Diagnostics.AddWarning("Entry already deleted", util.ErrorDetailFromContentfulManagementResponse(response, err))
+			return
+		}
+		resp.Diagnostics.AddError("Failed to delete entry", util.ErrorDetailFromContentfulManagementResponse(response, err))
+	}
 }
