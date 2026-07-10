dad8802d3e971c3197049f14e4f5dddbfdf6c6ff
Scott Talbot <s@chikachow.org>
2026-07-11 09:20:37 +1000

    test(provider): verify metadata plan reconciliation

--- internal/provider/resource_content_type_test.go
+++ internal/provider/resource_content_type_test.go
@@ -1,6 +1,7 @@
 package provider_test
 
 import (
+	"context"
 	"regexp"
 	"testing"
 
@@ -9,7 +10,11 @@ import (
 	"github.com/hashicorp/terraform-plugin-testing/config"
 	"github.com/hashicorp/terraform-plugin-testing/helper/acctest"
 	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+	"github.com/hashicorp/terraform-plugin-testing/knownvalue"
 	"github.com/hashicorp/terraform-plugin-testing/plancheck"
+	"github.com/hashicorp/terraform-plugin-testing/statecheck"
+	"github.com/hashicorp/terraform-plugin-testing/tfjsonpath"
+	"github.com/stretchr/testify/require"
 )
 
 func TestAccContentTypeResourceImport(t *testing.T) {
@@ -66,6 +71,72 @@ func TestAccContentTypeResourceImport(t *testing.T) {
 				ImportState:     true,
 				ImportStateId:   "0p38pssr0fi3/test/author",
 				// ImportStateVerify: true,
+			},
+		},
+	})
+}
+
+func TestAccContentTypeResourceImportWithTaxonomy(t *testing.T) {
+	t.Parallel()
+
+	server, _ := cmt.NewContentfulManagementServer()
+
+	server.SetContentType("0p38pssr0fi3", "test", "author", cm.ContentTypeRequestData{
+		Name:         "Author",
+		Description:  cm.NewOptNilString("An author"),
+		DisplayField: "name",
+		Fields:       []cm.ContentTypeRequestDataFieldsItem{},
+		Metadata: cm.NewOptContentTypeMetadata(cm.ContentTypeMetadata{
+			Taxonomy: []cm.ContentTypeMetadataTaxonomyItem{
+				{
+					Sys: cm.ContentTypeMetadataTaxonomyItemSys{
+						Type:     cm.ContentTypeMetadataTaxonomyItemSysTypeLink,
+						ID:       "furniture",
+						LinkType: cm.ContentTypeMetadataTaxonomyItemSysLinkTypeTaxonomyConceptScheme,
+					},
+				},
+				{
+					Sys: cm.ContentTypeMetadataTaxonomyItemSys{
+						Type:     cm.ContentTypeMetadataTaxonomyItemSysTypeLink,
+						ID:       "livingRoomFurniture",
+						LinkType: cm.ContentTypeMetadataTaxonomyItemSysLinkTypeTaxonomyConcept,
+					},
+				},
+			},
+		}),
+	})
+
+	configVariables := config.Variables{
+		"space_id":       config.StringVariable("0p38pssr0fi3"),
+		"environment_id": config.StringVariable("test"),
+	}
+
+	ContentfulProviderMockedResourceTest(t, server, resource.TestCase{
+		Steps: []resource.TestStep{
+			{
+				ConfigDirectory:    config.StaticDirectory("testdata/TestAccContentTypeResourceImport"),
+				ConfigVariables:    configVariables,
+				PlanOnly:           true,
+				ExpectNonEmptyPlan: true,
+			},
+			{
+				ConfigDirectory:    config.StaticDirectory("testdata/TestAccContentTypeResourceImport"),
+				ConfigVariables:    configVariables,
+				ResourceName:       "contentful_content_type.author",
+				ImportState:        true,
+				ImportStateId:      "0p38pssr0fi3/test/author",
+				ImportStatePersist: true,
+				ConfigStateChecks:  contentTypeTaxonomyStateChecks("contentful_content_type.author"),
+			},
+			{
+				ConfigDirectory: config.StaticDirectory("testdata/TestAccContentTypeResourceImport"),
+				ConfigVariables: configVariables,
+				ConfigPlanChecks: resource.ConfigPlanChecks{
+					PreApply: []plancheck.PlanCheck{
+						plancheck.ExpectResourceAction("contentful_content_type.author", plancheck.ResourceActionNoop),
+					},
+				},
+				ConfigStateChecks: contentTypeTaxonomyStateChecks("contentful_content_type.author"),
 			},
 		},
 	})
@@ -245,6 +316,16 @@ func TestAccContentTypeResourceUpdateMetadata(t *testing.T) {
 				},
 			},
 			{
+				ConfigDirectory: config.StaticDirectory("testdata/TestAccContentTypeResourceUpdateMetadata/annotations_only"),
+				ConfigVariables: configVariables,
+				ConfigPlanChecks: resource.ConfigPlanChecks{
+					PreApply: []plancheck.PlanCheck{
+						plancheck.ExpectResourceAction("contentful_content_type.test", plancheck.ResourceActionNoop),
+					},
+				},
+				ConfigStateChecks: contentTypeTaxonomyStateChecks("contentful_content_type.test"),
+			},
+			{
 				ConfigDirectory: config.StaticDirectory("testdata/TestAccContentTypeResourceUpdateMetadata/1"),
 				ConfigVariables: configVariables,
 				ConfigPlanChecks: resource.ConfigPlanChecks{
@@ -252,6 +333,7 @@ func TestAccContentTypeResourceUpdateMetadata(t *testing.T) {
 						plancheck.ExpectResourceAction("contentful_content_type.test", plancheck.ResourceActionUpdate),
 					},
 				},
+				ConfigStateChecks: contentTypeMetadataWithoutAnnotationsStateChecks(),
 			},
 			{
 				ConfigDirectory: config.StaticDirectory("testdata/TestAccContentTypeResourceUpdateMetadata/1"),
@@ -261,6 +343,7 @@ func TestAccContentTypeResourceUpdateMetadata(t *testing.T) {
 						plancheck.ExpectResourceAction("contentful_content_type.test", plancheck.ResourceActionNoop),
 					},
 				},
+				ConfigStateChecks: contentTypeMetadataWithoutAnnotationsStateChecks(),
 			},
 			{
 				ConfigDirectory: config.StaticDirectory("testdata/TestAccContentTypeResourceUpdateMetadata/3"),
@@ -282,6 +365,128 @@ func TestAccContentTypeResourceUpdateMetadata(t *testing.T) {
 			},
 		},
 	})
+}
+
+func contentTypeTaxonomyStateChecks(resourceAddress string) []statecheck.StateCheck {
+	return []statecheck.StateCheck{
+		statecheck.ExpectKnownValue(
+			resourceAddress,
+			tfjsonpath.New("metadata").AtMapKey("taxonomy").AtSliceIndex(0).AtMapKey("taxonomy_concept_scheme").AtMapKey("id"),
+			knownvalue.StringExact("furniture"),
+		),
+		statecheck.ExpectKnownValue(
+			resourceAddress,
+			tfjsonpath.New("metadata").AtMapKey("taxonomy").AtSliceIndex(1).AtMapKey("taxonomy_concept").AtMapKey("id"),
+			knownvalue.StringExact("livingRoomFurniture"),
+		),
+	}
+}
+
+func contentTypeMetadataWithoutAnnotationsStateChecks() []statecheck.StateCheck {
+	return append(contentTypeTaxonomyStateChecks("contentful_content_type.test"), statecheck.ExpectKnownValue(
+		"contentful_content_type.test",
+		tfjsonpath.New("metadata").AtMapKey("annotations"),
+		knownvalue.Null(),
+	))
+}
+
+func TestAccContentTypeResourceTaxonomyDrift(t *testing.T) {
+	t.Parallel()
+
+	server, _ := cmt.NewContentfulManagementServer()
+	server.RegisterSpaceEnvironment("0p38pssr0fi3", "test")
+
+	contentTypeID := "acctest_" + acctest.RandStringFromCharSet(8, "abcdefghijklmnopqrstuvwxyz")
+	configVariables := config.Variables{
+		"space_id":             config.StringVariable("0p38pssr0fi3"),
+		"environment_id":       config.StringVariable("test"),
+		"test_content_type_id": config.StringVariable(contentTypeID),
+	}
+
+	ContentfulProviderMockedResourceTest(t, server, resource.TestCase{
+		Steps: []resource.TestStep{
+			{
+				ConfigDirectory: config.StaticDirectory("testdata/TestAccContentTypeResourceUpdateMetadata/1"),
+				ConfigVariables: configVariables,
+			},
+			{
+				PreConfig: func() {
+					setContentTypeTaxonomyDrift(t, server, contentTypeID, []cm.ContentTypeMetadataTaxonomyItem{
+						contentTypeTaxonomyConceptScheme("furniture"),
+					})
+				},
+				ConfigDirectory: config.StaticDirectory("testdata/TestAccContentTypeResourceUpdateMetadata/1"),
+				ConfigVariables: configVariables,
+				ConfigPlanChecks: resource.ConfigPlanChecks{
+					PreApply: []plancheck.PlanCheck{
+						plancheck.ExpectResourceAction("contentful_content_type.test", plancheck.ResourceActionNoop),
+					},
+				},
+				ConfigStateChecks: []statecheck.StateCheck{
+					statecheck.ExpectKnownValue(
+						"contentful_content_type.test",
+						tfjsonpath.New("metadata").AtMapKey("taxonomy"),
+						knownvalue.ListSizeExact(1),
+					),
+				},
+			},
+			{
+				ConfigDirectory: config.StaticDirectory("testdata/TestAccContentTypeResourceUpdateMetadata/2"),
+				ConfigVariables: configVariables,
+				ConfigPlanChecks: resource.ConfigPlanChecks{
+					PreApply: []plancheck.PlanCheck{
+						plancheck.ExpectResourceAction("contentful_content_type.test", plancheck.ResourceActionUpdate),
+					},
+				},
+				ConfigStateChecks: contentTypeTaxonomyStateChecks("contentful_content_type.test"),
+			},
+			{
+				PreConfig: func() {
+					setContentTypeTaxonomyDrift(t, server, contentTypeID, []cm.ContentTypeMetadataTaxonomyItem{})
+				},
+				ConfigDirectory: config.StaticDirectory("testdata/TestAccContentTypeResourceUpdateMetadata/2"),
+				ConfigVariables: configVariables,
+				ConfigPlanChecks: resource.ConfigPlanChecks{
+					PreApply: []plancheck.PlanCheck{
+						plancheck.ExpectResourceAction("contentful_content_type.test", plancheck.ResourceActionUpdate),
+					},
+				},
+				ConfigStateChecks: contentTypeTaxonomyStateChecks("contentful_content_type.test"),
+			},
+		},
+	})
+}
+
+func setContentTypeTaxonomyDrift(
+	t *testing.T,
+	server *cmt.Server,
+	contentTypeID string,
+	taxonomy []cm.ContentTypeMetadataTaxonomyItem,
+) {
+	t.Helper()
+
+	response, err := server.Handler().GetContentType(context.Background(), cm.GetContentTypeParams{
+		SpaceID: "0p38pssr0fi3", EnvironmentID: "test", ContentTypeID: contentTypeID,
+	})
+	require.NoError(t, err)
+
+	contentType, ok := response.(*cm.ContentType)
+	require.True(t, ok)
+
+	metadata := contentType.Metadata.Or(cm.ContentTypeMetadata{})
+	metadata.Taxonomy = taxonomy
+	contentType.Metadata.SetTo(metadata)
+	contentType.Sys.Version++
+}
+
+func contentTypeTaxonomyConceptScheme(id string) cm.ContentTypeMetadataTaxonomyItem {
+	return cm.ContentTypeMetadataTaxonomyItem{
+		Sys: cm.ContentTypeMetadataTaxonomyItemSys{
+			Type:     cm.ContentTypeMetadataTaxonomyItemSysTypeLink,
+			ID:       id,
+			LinkType: cm.ContentTypeMetadataTaxonomyItemSysLinkTypeTaxonomyConceptScheme,
+		},
+	}
 }
 
 //nolint:paralleltest
