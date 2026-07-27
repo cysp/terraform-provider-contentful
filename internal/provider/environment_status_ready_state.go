b48a4fa86e3c5c5b45957c387fd3dd001f7413fb
Scott Talbot <s@chikachow.org>
2026-07-27 14:47:08 +1000

    fix(environment-status): publish converted state atomically

---
+++ internal/provider/environment_status_ready_state.go
@@ -0,0 +1,42 @@
+package provider
+
+import (
+	"context"
+
+	cm "github.com/cysp/terraform-provider-contentful/internal/contentful-management-go"
+	datasourcetimeouts "github.com/hashicorp/terraform-plugin-framework-timeouts/datasource/timeouts"
+	"github.com/hashicorp/terraform-plugin-framework/diag"
+	"github.com/hashicorp/terraform-plugin-framework/tfsdk"
+)
+
+func publishEnvironmentStatusReadyResponse(
+	ctx context.Context,
+	state *tfsdk.State,
+	response cm.Environment,
+	configuredTimeouts datasourcetimeouts.Value,
+) (bool, diag.Diagnostics) {
+	model, diags := NewEnvironmentStatusReadyModelFromResponse(ctx, response)
+
+	return publishEnvironmentStatusReadyConversion(ctx, state, configuredTimeouts, model, diags)
+}
+
+func publishEnvironmentStatusReadyConversion(
+	ctx context.Context,
+	state *tfsdk.State,
+	configuredTimeouts datasourcetimeouts.Value,
+	model EnvironmentStatusReadyModel,
+	diags diag.Diagnostics,
+) (bool, diag.Diagnostics) {
+	if diags.HasError() {
+		return false, diags
+	}
+
+	model.Timeouts = configuredTimeouts
+	diags.Append(state.Set(ctx, &model)...)
+
+	if diags.HasError() {
+		return false, diags
+	}
+
+	return model.Status.ValueString() != environmentStatusReadyValue, diags
+}
