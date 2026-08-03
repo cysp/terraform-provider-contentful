566b391bd141cef393b5f05b68f8a4e699a1f813
Scott Talbot <s@chikachow.org>
2026-08-04 08:56:39 +1000

    fix(app): reject unresolved request values

---
+++ internal/provider/app_request_string.go
@@ -0,0 +1,49 @@
+package provider
+
+import (
+	"github.com/hashicorp/terraform-plugin-framework/diag"
+	"github.com/hashicorp/terraform-plugin-framework/path"
+	"github.com/hashicorp/terraform-plugin-framework/types"
+)
+
+func appRequestRequiredString(value types.String, valuePath path.Path) (string, diag.Diagnostics) {
+	diags := diag.Diagnostics{}
+
+	switch {
+	case value.IsUnknown():
+		diags.AddAttributeError(
+			valuePath,
+			"Unexpected unknown request value",
+			"This value must be known before it can be sent to Contentful.",
+		)
+	case value.IsNull():
+		diags.AddAttributeError(
+			valuePath,
+			"Unexpected null request value",
+			"This value cannot be null when it is sent to Contentful.",
+		)
+	default:
+		return value.ValueString(), diags
+	}
+
+	return "", diags
+}
+
+func appRequestOptionalString(value types.String, valuePath path.Path) (*string, diag.Diagnostics) {
+	diags := diag.Diagnostics{}
+
+	switch {
+	case value.IsUnknown():
+		diags.AddAttributeError(
+			valuePath,
+			"Unexpected unknown request value",
+			"This value must be known before it can be sent to Contentful.",
+		)
+	case value.IsNull():
+		return nil, diags
+	default:
+		return value.ValueStringPointer(), diags
+	}
+
+	return nil, diags
+}
