6f9b0366096a7c0c3ff9a726926d56f8fb69ef52
Scott Talbot <s@chikachow.org>
2026-08-04 08:53:43 +1000

    fix(entry): reject unresolved request values

--- internal/provider/testdata/entry_model.go
+++ internal/provider/testdata/entry_model.go
@@ -15,23 +15,21 @@ func EntryModel(spaceID, environmentID, contentTypeID, entryID string) *rapid.Ge
 			IDIdentityModel:    provider.NewIDIdentityModelFromMultipartID(spaceID, environmentID, entryID),
 			EntryIdentityModel: provider.NewEntryIdentityModel(spaceID, environmentID, entryID),
 			ContentTypeID:      types.StringValue(contentTypeID),
-			Fields: RandomZeroable(
-				rapid.Map(
-					rapid.MapOfN(
-						AlphanumericStringOfN(1, 10),
-						rapid.Map(
-							rapid.Custom(func(t *rapid.T) string {
-								value := rapid.String().Draw(t, "value")
-								bytes, _ := json.Marshal(value)
-								return string(bytes)
-							}),
-							jsontypes.NewNormalizedValue,
-						),
-						0,
-						5,
+			Fields: rapid.Map(
+				rapid.MapOfN(
+					AlphanumericStringOfN(1, 10),
+					rapid.Map(
+						rapid.Custom(func(t *rapid.T) string {
+							value := rapid.String().Draw(t, "value")
+							bytes, _ := json.Marshal(value)
+							return string(bytes)
+						}),
+						jsontypes.NewNormalizedValue,
 					),
-					provider.NewTypedMap,
+					0,
+					5,
 				),
+				provider.NewTypedMap,
 			).Draw(t, "fields"),
 			Metadata: RandomZeroable(
 				rapid.Map(
