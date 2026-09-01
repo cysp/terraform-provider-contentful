e1a18e21c2533cf197729d8dada05350ae7ff787
Scott Talbot <s@chikachow.org>
2026-09-02 08:38:14 +1000

    refactor(provider): remove unused scaffolding

--- internal/provider/attrs_test.go
+++ internal/provider/attrs_test.go
@@ -1,4 +1,4 @@
-package provider
+package provider_test
 
 import (
 	"context"
@@ -7,12 +7,6 @@ import (
 	"github.com/hashicorp/terraform-plugin-framework/diag"
 	"github.com/hashicorp/terraform-plugin-framework/types/basetypes"
 )
-
-type AttrTypeWithValueFromObject interface {
-	attr.Type
-
-	ValueFromObject(ctx context.Context, value basetypes.ObjectValue) (basetypes.ObjectValuable, diag.Diagnostics)
-}
 
 type AttrValueWithToObjectValue interface {
 	attr.Value
