1d0399b4e16f12b8dee800d983c5a4085d042b6d
Scott Talbot <s@chikachow.org>
2026-09-05 21:53:54 +1000

    docs(editor-interface): align import address with example

--- examples/resources/contentful_editor_interface/import-by-identity.tf
+++ examples/resources/contentful_editor_interface/import-by-identity.tf
@@ -4,5 +4,5 @@ import {
     environment_id  = var.contentful_environment_id
     content_type_id = var.content_type_id
   }
-  to = contentful_editor_interface.this
+  to = contentful_editor_interface.author
 }
