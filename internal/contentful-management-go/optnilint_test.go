a3f4605fe73f50a57fc7898e8a4a71dfd9881c6a
Scott Talbot <s@chikachow.org>
2026-09-05 17:17:22 +1000

    refactor(contentful-management-go): remove unused nullable integer conversions

--- internal/contentful-management-go/optnilint_test.go
+++ internal/contentful-management-go/optnilint_test.go
@@ -7,38 +7,6 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestNewOptNilPointerInt(t *testing.T) {
-	t.Parallel()
-
-	tests := map[string]struct {
-		input    *int
-		expected cm.OptNilInt
-	}{
-		"test": {
-			input:    new(42),
-			expected: cm.NewOptNilInt(42),
-		},
-		"zero": {
-			input:    new(0),
-			expected: cm.NewOptNilInt(0),
-		},
-		"nil": {
-			input:    nil,
-			expected: cm.NewOptNilIntNull(),
-		},
-	}
-
-	for name, test := range tests {
-		t.Run(name, func(t *testing.T) {
-			t.Parallel()
-
-			actual := cm.NewOptNilPointerInt(test.input)
-
-			assert.Equal(t, test.expected, actual)
-		})
-	}
-}
-
 func TestNewOptNilPointerInt64(t *testing.T) {
 	t.Parallel()
 
@@ -65,78 +33,6 @@ func TestNewOptNilPointerInt64(t *testing.T) {
 			t.Parallel()
 
 			actual := cm.NewOptNilPointerInt64(test.input)
-
-			assert.Equal(t, test.expected, actual)
-		})
-	}
-}
-
-func TestOptNilIntValueIntPointer(t *testing.T) {
-	t.Parallel()
-
-	tests := map[string]struct {
-		input    cm.OptNilInt
-		expected *int
-	}{
-		"test": {
-			input:    cm.NewOptNilInt(42),
-			expected: new(42),
-		},
-		"zero": {
-			input:    cm.NewOptNilInt(0),
-			expected: new(0),
-		},
-		"null": {
-			input:    cm.NewOptNilIntNull(),
-			expected: nil,
-		},
-		"nil": {
-			input:    cm.OptNilInt{},
-			expected: nil,
-		},
-	}
-
-	for name, test := range tests {
-		t.Run(name, func(t *testing.T) {
-			t.Parallel()
-
-			actual := test.input.ValueIntPointer()
-
-			assert.Equal(t, test.expected, actual)
-		})
-	}
-}
-
-func TestOptNilIntValueInt64Pointer(t *testing.T) {
-	t.Parallel()
-
-	tests := map[string]struct {
-		input    cm.OptNilInt
-		expected *int64
-	}{
-		"test": {
-			input:    cm.NewOptNilInt(42),
-			expected: new(int64(42)),
-		},
-		"zero": {
-			input:    cm.NewOptNilInt(0),
-			expected: new(int64(0)),
-		},
-		"null": {
-			input:    cm.NewOptNilIntNull(),
-			expected: nil,
-		},
-		"nil": {
-			input:    cm.OptNilInt{},
-			expected: nil,
-		},
-	}
-
-	for name, test := range tests {
-		t.Run(name, func(t *testing.T) {
-			t.Parallel()
-
-			actual := test.input.ValueInt64Pointer()
 
 			assert.Equal(t, test.expected, actual)
 		})
