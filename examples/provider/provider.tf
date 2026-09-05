7b10f61ef65ca44aab6e24245aeba72ebba1d7ab
Scott Talbot <s@chikachow.org>
2026-09-05 20:19:03 +1000

    docs(provider): make first example complete

--- examples/provider/provider.tf
+++ examples/provider/provider.tf
@@ -7,3 +7,21 @@ terraform {
 }
 
 provider "contentful" {}
+
+variable "contentful_space_id" {
+  type = string
+}
+
+variable "contentful_environment_id" {
+  type    = string
+  default = "master"
+}
+
+resource "contentful_tag" "example" {
+  space_id       = var.contentful_space_id
+  environment_id = var.contentful_environment_id
+  tag_id         = "terraform-example"
+
+  name       = "Terraform example"
+  visibility = "private"
+}
